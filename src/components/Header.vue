51da2176a246a428bbca51f6b650f5f5a71285e3
Yousef_Maher <yousef.maher.c@stc.com.kw>
2022-01-08 11:34:51 +0200

    Handle main style with scss and handle the routes needed

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/addItem">Add item to inventory</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/AddItem.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="add-item">
+    <h1>Add item Under construction</h1>
+  </div>
+</template>
